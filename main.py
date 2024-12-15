7cba61a965cfbcfe573d7951fdf35309c66bf79a
Ocean-Current <53261018+phhandong@users.noreply.github.com>
2024-12-15 16:51:11 +0800

    依次print key

--- main.py
+++ main.py
@@ -130,6 +130,8 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true', help='Debug mode')
     args = parser.parse_args()
     print(args)
+    for i in args.zotero_key:
+      print(i)
     exit()
     assert args.zotero_id is not None
     assert args.zotero_key is not None
